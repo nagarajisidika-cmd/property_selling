e952207ef637aaa5f3edc0e43f876bb661657881
nagarajisidika <nagarajisidika@gmail.com>
2026-07-29 11:38:24 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,61 @@
+<html>
+<head>
+<style>
+body{
+    margin:20;
+    font-family:Arial;
+    background:linear-gradient(to right, #4facfe, #00f2fe);
+    text-align:center;
+}
+ #4facfe
+h1{
+    color:white;
+    margin-top:50px;
+    font-size:45px;
+    text-shadow:2px 2px 5px black;
+}
+
+table{
+    margin-top:80px;
+}
+
+th{
+    background:white;
+    border-radius:15px;
+    padding:30px;
+    width:250px;
+    height:150px;
+    box-shadow:0px 0px 15px gray;
+    transition:0.3s;
+}
+
+th:hover{
+    background:#003366;
+}
+
+th a{
+    text-decoration:none;
+    color:#003366;
+    font-size:25px;
+    font-weight:bold;
+}
+
+th:hover a{
+    color:white;
+}
+</style>
+</head>
+
+<body>
+
+<h1>📚 Siya Library</h1>
+
+<table cellpadding="20" align="center" cellspacing="20">
+<tr>
+<th><a href="login.php">ADMIN</a></th>
+<th><a href="login2.php">STUDENT</a></th>
+</tr>
+</table>
+
+</body>
+</html>
